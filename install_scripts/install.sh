66a702fea387754a31f381ee5bebe8d297d679af
Yuantai Du <yuantai.du@import.io>
2023-01-07 20:38:56 -0500

    tested with g4dn

--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -5,28 +5,40 @@
 cd
 sudo yum update -y
 sudo yum groupinstall "Development Tools" -y
+sudo yum install openmpi fftw-devel -y
 
-cat >> $HOME/.bashrc <<EOF
 # Charmm stuff
-export PATH=$PATH:$HOME/.local/bin:$HOME/bin:$HOME/charmm/bin:/usr/local/bin:/usr/lib64/openmpi/bin:$HOME/miniconda3/bin
+export PATH=$PATH:$HOME/.local/bin:$HOME/bin:/opt/nvidia/cuda/bin:$HOME/charmm/bin:/usr/local/bin:/usr/lib64/openmpi/bin:$HOME/miniconda3/bin
 export OPENMM_PLUGIN_DIR=$HOME/miniconda3/pkgs/openmm/lib/plugins
 export LD_LIBRARY_PATH=$HOME/miniconda3/pkgs/openmm/lib
 export CUDATK=/opt/nvidia/cuda
 # End Charmm stuff
-EOF
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash ./Miniconda3-latest-Linux-x86_64.sh -b
+# install anaconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash ./Miniconda3-latest-Linux-x86_64.sh -bt && rm Miniconda3-latest-Linux-x86_64.sh
 $HOME/miniconda3/bin/conda init bash
-
 . $HOME/.bashrc
 
+
+conda create -n openmm-env -y
+cat > /tmp/install2 << EOF2
+conda activate openmm-env
 conda install -c conda-forge openmm cudatoolkit=9.2 -y
+python -m openmm.testInstallation
+EOF2
+. /tmp/install2
+
+ln -s $(basename $(ls -d $HOME/miniconda3/pkgs/openmm*/)) $HOME/miniconda3/pkgs/openmm
 
 [ -d $HOME/bin ] || mkdir $HOME/bin
 aws s3 sync s3://annadu/charmm/bin/ $HOME/bin
 
 aws s3 cp s3://annadu/charmm/c47b1.tar.gz .
-tar xfz c47b1.tar.gz
+tar xfz c47b1.tar.gz && rm c47b1.tar.gz
 cd charmm
+cat > /tmp/install3 << EOF3
+conda activate openmm-env
 ./configure > configure.out 2>&1
 make -C build/cmake install > build.log 2>&1
+EOF3
+. /tmp/install3
--- lib/user-data.sh
+++ lib/user-data.sh
@@ -1,3 +1,7 @@
-aws s3 cp s3://annadu/charmm/install_scripts/install.sh /
-chmod a+r /install.sh
-su ec2-user -c 'bash /install.sh'
+yum update -y
+yum groupinstall "Development Tools" -y
+yum install openmpi fftw-devel -y
+
+aws s3 cp s3://annadu/charmm/install_scripts/install.sh /home/ec2-user/install_scripts/install.sh
+chown ec2-user:ec2-user /home/ec2-user/install_scripts/install.sh /home/ec2-user/install_scripts/
+su ec2-user -c 'bash /home/ec2-user/install_scripts/install.sh'
