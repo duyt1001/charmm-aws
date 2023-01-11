41ac36e462c5e9a5ff36690bdbe97c5d3564b0d6
Yuantai Du <yuantai.du@import.io>
2023-01-11 17:46:22 -0500

    add scripts

---
+++ scripts/continue-steps.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+CNTS="${1:-6}"	# how many cnt (iterations) of stepN_production
+SIM=$2
+#DRYRUN=1	# comment or set to empty to real run
+#
+# Usage: continue-steps.sh <cnts> <simulation>
+
+prev_last_cnt() {
+  # look for step5_3.pdb
+  done_pdbs=$(ls step[0-9]*_[0-9]*.pdb 2>/dev/null)
+  [ x"$done_pdbs" = x ] && echo 0 && return
+  last_pdb=$(awk '{print $NF}' <<< $done_pdbs)
+  echo $last_pdb | awk -F_ '{print $2}' | awk -F. '{print $1}'
+}
+
+# is there another simulation?
+CurrentSimFile=~/.current_simulation
+if [ -f $CurrentSimFile ] ; then
+  echo "Currently running another sinulation $(cat $CurrentSimFile)" >&2
+  exit 1
+fi
+
+# set current simulation
+if [ x"$SIM" != x ] ; then
+  [ ! -d $HOME/$SIM ] && echo "$HOME/$SIM not found" >&2 && exit 1
+  cd $HOME/$SIM/charmm_openmm
+else
+  if [ $(basename $PWD) = "charmm_openmm" ] ; then
+    SIM=$(cd .. ; basename $PWD)
+  elif [ -d ./charmm_openmm ] ; then
+    SIM=$(basename $PWD)
+    cd charmm_openmm
+  fi
+fi
+
+echo $SIM > ~/.current_simulation
+
+# continue after previous steps
+prev=$(prev_last_cnt) # previously cnt ran to this
+inp=$(ls *production.inp)
+echo "Previously finished on $prev steps, will continue until $CNTS steps"
+# Last stepN_production.inp
+for cnt in `seq $((prev +1)) $CNTS` ; do
+      outfile=${inp}.cnt=${cnt}.out
+      echo -n "${PWD//$HOME\//}> "
+      echo " mpirun charmm cnt=$cnt -i $inp -o $outfile"
+      [ -z $DRYRUN ] && mpirun charmm cnt=$cnt -i $inp -o $outfile
+      tail -7 $outfile
+done
+
+
+# All done, archive results and send to s3
+cd .. 
+SIM=$(basename $PWD)
+RESULTS=${SIM}_results.tgz
+cd ..
+echo "tar cfz $RESULTS $SIM"
+[ -z $DRYRUN ] && tar cfz $RESULTS $SIM
+echo aws s3 cp $RESULTS s3://annadu/charmm/results/$RESULTS
+[ -z $DRYRUN ] && aws s3 cp $RESULTS s3://annadu/charmm/results/$RESULTS
+
+rm $CurrentSimFile
---
+++ scripts/run-charmm-steps.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+SIM=${1:-help}
+CNTS=3	# how many cnt (iterations) of stepN_production
+#DRYRUN=1	# comment or set to empty to real run
+
+# find the workspace:
+# 1. current folder
+# 2. ./$SIM folder
+# 3. has $SIM.tgz
+SIM=${SIM%.tgz}
+if [ $SIM = "help" ] || [ $SIM = "-h" ] || [ $SIM = "--help" ] ; then
+    echo "Usage: `basename $0` <Simulation>" && exit
+elif [ $SIM = "." ] ; then
+    SIM=$(basename $PWD)
+elif [ $SIM = $(basename $PWD) ] && [ $PWD != $HOME ] ; then
+    :
+elif [ -d "$SIM" ] ; then
+    cd $SIM
+elif [ -f "${SIM}.tgz" ] ; then
+    tar xfz "${SIM}.tgz"
+    mv charmm-gui-* "${SIM}"
+    cd $SIM
+else
+    echo "Unable to find the simulation $SIM" >&2
+    exit 1
+fi
+pwd
+
+CurrentSimFile=~/.current_simulation
+if [ -f $CurrentSimFile ] ; then
+  echo "Currently running another sinulation $(cat $CurrentSimFile)" >&2
+  exit 1
+fi
+
+echo $SIM > ~/.current_simulation
+
+# charmm gui's input files are in order
+for inp in *.inp
+do
+    if [ "$inp" = *"production.inp" ] ; then
+	break
+    fi
+
+    outfile=${inp}.out
+    if [ -f charmm_openmm/$inp ] ; then 
+        cd charmm_openmm
+	echo -n "${PWD//$HOME\//}> "
+        echo " mpirun charmm -i $inp -o $outfile"
+	[ -z $DRYRUN ] && mpirun charmm -i $inp -o $outfile
+	tail -7 $outfile
+	cd ..
+    else
+	echo -n "${PWD//$HOME\//}> "
+        echo " charmm -i $inp -o $outfile"
+        [ -z $DRYRUN ] && charmm -i $inp -o $outfile
+	tail -7 $outfile
+    fi
+done
+
+# Last stepN_production.inp
+if [ $inp = *"production.inp" ] ; then
+    cd charmm_openmm
+    for cnt in `seq 1 $CNTS` ; do
+      outfile=${inp}.cnt=${cnt}.out
+      echo -n "${PWD//$HOME\//}> "
+      echo " mpirun charmm cnt=$cnt -i $inp -o $outfile"
+      [ -z $DRYRUN ] && mpirun charmm cnt=$cnt -i $inp -o $outfile
+      tail -7 $outfile
+    done
+    cd .. 
+fi
+
+# All done, archive results and send to s3
+RESULTS=${SIM}_results.tgz
+cd ..
+echo "tar cfz $RESULTS $SIM"
+[ -z $DRYRUN ] && tar cfz $RESULTS $SIM
+echo aws s3 cp $RESULTS s3://annadu/charmm/results/$RESULTS
+[ -z $DRYRUN ] && aws s3 cp $RESULTS s3://annadu/charmm/results/$RESULTS
+
+rm $CurrentSimFile
